e637427e5e0b3491d87d91ffec9c780633a36a04
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-01-13 15:19:57 -0500

    ran black

--- motion_glow.py
+++ motion_glow.py
@@ -4,10 +4,12 @@ from cv2utils.camera import make_camera_with_args
 
 thresh = 5
 
+
 def prepare(frame):
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     frame = cv2.GaussianBlur(frame, (21, 21), 0)
     return frame
+
 
 def preprocess(frames, raw):
     img = frames[1]
@@ -16,10 +18,7 @@ def preprocess(frames, raw):
     diff[diff > thresh] = 1
     return cv2.add(cv2.cvtColor(diff, cv2.COLOR_GRAY2BGR) * raw, raw)
 
+
 parser = make_parser()
 camera, args = make_camera_with_args(parser=parser, log=False, fps=15, res=(1280, 720))
-camera.make_virtual_webcam(
-    prepare=prepare,
-    preprocess=preprocess,
-    frames_stored=2
-)
+camera.make_virtual_webcam(prepare=prepare, preprocess=preprocess, frames_stored=2)
--- template.py
+++ template.py
@@ -2,18 +2,15 @@ import cv2
 from cv2utils.args import make_parser
 from cv2utils.camera import make_camera_with_args
 
-thresh = 5
 
 def prepare(frame):
     return frame
 
+
 def preprocess(frames, raw):
     return frames[0]
 
+
 parser = make_parser()
 camera, args = make_camera_with_args(parser=parser, log=False, fps=15, res=(1280, 720))
-camera.make_virtual_webcam(
-    prepare=prepare,
-    preprocess=preprocess,
-    frames_stored=1
-)
+camera.make_virtual_webcam(prepare=prepare, preprocess=preprocess, frames_stored=1)
