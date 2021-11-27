99930a5849e776c2bf88136900be4fcece178b29
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-11-27 00:22:23 -0500

    webcam not stream

--- motion_glow.py
+++ motion_glow.py
@@ -25,4 +25,4 @@ parser.add_argument(
 )
 camera, args = make_camera_with_args(parser=parser, log=False, fps=15, res=(1280, 720))
 thresh = args.threshold
-camera.stream(prepare=prepare, preprocess=preprocess, frames_stored=2)
+camera.make_virtual_webcam(prepare=prepare, preprocess=preprocess, frames_stored=2)
